03520e12341eb1dad0d96e268ea04262a0e6117b
Craig Heydenburg <craigh@mac.com>
2022-05-20 13:46:18 -0400

    add example of altering the formtype list

---
+++ src/EventSubscriber/CustomFormTypeSubscriber.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\EventSubscriber;
+
+use App\Form\HairColorType;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Zikula\Bundle\DynamicFormPropertyBundle\Event\FormTypeChoiceEvent;
+
+class CustomFormTypeSubscriber implements EventSubscriberInterface
+{
+    public static function getSubscribedEvents(): array
+    {
+        return [
+            FormTypeChoiceEvent::class => ['addFormTypes'],
+        ];
+    }
+
+    public function addFormTypes(FormTypeChoiceEvent $event)
+    {
+        $event->addChoice('Other fields', 'Hair Color', HairColorType::class);
+        $event->removeChoice('Choice fields', 'Currency');
+    }
+}
---
+++ src/Form/HairColorType.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class HairColorType extends AbstractType
+{
+    public function getParent()
+    {
+        return ChoiceType::class;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'choices' => [
+                'Black' => 'black',
+                'Blonde' => 'blonde',
+                'Brown' => 'brown',
+                'Red' => 'red',
+            ],
+        ]);
+    }
+}
