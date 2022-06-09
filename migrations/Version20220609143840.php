f29cc3b776d946be17b8eb5e84cff725dd5b0187
Craig Heydenburg <craigh@mac.com>
2022-06-09 16:51:53 -0400

    display using groups

---
+++ migrations/Version20220609143840.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20220609143840 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE question ADD groups TEXT DEFAULT \'a:0:{}\' NOT NULL');
+        $this->addSql('COMMENT ON COLUMN question.groups IS \'(DC2Type:array)\'');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE SCHEMA public');
+        $this->addSql('ALTER TABLE question DROP groups');
+    }
+}
